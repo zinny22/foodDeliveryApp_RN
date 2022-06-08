9c07a441bb4ad9cdf8c6c3282cba59bce6b7e49c
ziny <ziny@zinyui-MacBookPro.local>
2022-06-08 20:35:51 +0900

    :construction: navigation

--- App.tsx
+++ App.tsx
@@ -4,27 +4,39 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View} from 'react-native';
+import {
+  Text,
+  TouchableHighlight,
+  Pressable,
+  Button,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  View} from 'react-native';
 import {useCallback} from 'react';
 
 type RootStackParamList = {
   Home: undefined;
   Details: undefined;
 };
+//타입스트립트에서 네비게이션 선언하는 방법 그냥 외우자 ㅎ
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
-function HomeScreen({navigation}: HomeScreenProps) {
+function HomeScreen({navigation, route}: HomeScreenProps) {
   const onClick = useCallback(() => {
     navigation.navigate('Details');
   }, [navigation]);
 
   return (
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableHighlight onPress={onClick}>
-              <Text>Home Screen</Text>
-            </TouchableHighlight>
+          <>
+          <View style={{flex: 1, backgroundColor:'orange' ,alignItems: 'flex-end', justifyContent: 'center'}}>
+            <Pressable onPress={onClick} style={{padding:20, backgroundColor:'white'}}>
+              <Text style={{color:'gray'}}>Home Screen</Text>
+            </Pressable>
           </View>
+          <View style={{backgroundColor:'green', flex:1}}><Text>second</Text></View>
+          </>
   );
 }
 
@@ -33,24 +45,45 @@ function DetailsScreen({navigation}: DetailsScreenProps) {
     navigation.navigate('Home');
   }, [navigation]);
 
+
   return (
+          <>
           <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <TouchableHighlight onPress={onClick}>
               <Text>Details Screen</Text>
             </TouchableHighlight>
           </View>
+          <View style={{
+            position:'absolute',
+            backgroundColor:'rgba(0,0,0,0.5)',
+            top:0,
+            bottom:0,
+            right:0,
+            left:0}}>
+              <View style={{
+                position:'absolute',
+                backgroundColor:'white',
+                top:50,
+                bottom:50,
+                right:50,
+                left:50}}>
+                <Text>hello</Text>
+              </View>
+          </View>
+          </>
   );
 }
 
 const Stack = createNativeStackNavigator();
 function App() {
   return (
+          //safe-area가 이미 적용 되어있다.
           <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
               <Stack.Screen
                       name="Home"
                       component={HomeScreen}
-                      options={{title: 'Overview'}}
+                      options={{title: '홈화면'}}
               />
               <Stack.Screen name="Details">
                 {props => <DetailsScreen {...props} />}
@@ -59,5 +92,6 @@ function App() {
           </NavigationContainer>
   );
 }
+
 
 export default App;
